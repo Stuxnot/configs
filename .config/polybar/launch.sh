dbcc8c6746be3382748ea0c8d57d80a0bd82bde5
stuxnot <heartbleed@tutanota.com>
2020-05-17 16:56:01 +0200

    Adds polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+exec polybar --reload desktop &
+if [[ ! -z $SIDE_MONITOR ]]; then
+    exec polybar --reload side &
+fi
